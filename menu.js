463678162ffbba23fc35b3005d8ef7858e624e40
Ameka Brown <brownameka05@gmail.com>
2018-10-10 20:13:04 -0500

    menu

---
+++ menu.js
@@ -0,0 +1,18 @@
+let menuList = document.getElementById("menuList")
+
+for(let index = 0; index < dishes.length; index++) {
+  let dish = dishes[index]
+
+let menuItem = `
+<li>
+<label>${dish.title}</label>
+<h4>${dish.description}</h4>
+<p>${dish.price}</p>
+<img scr= "${dish.imageURL}">
+</li>
+`
+
+menuList.insertAdjacentHTML('beforeend' ,menuItem)
+
+
+}
